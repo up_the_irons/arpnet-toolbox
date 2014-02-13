c1b95cbf7761cb52a05779c12ae486b7159b60d5
Garry Dolley <gdolley@arpnetworks.com>
2014-02-12 18:38:33 -0800

    An example

--- mail_reader.rb
+++ mail_reader.rb
@@ -5,7 +5,18 @@ require 'mail'
 # message to a block.  Uses the Mail gem.
 class MailReader
   # If a block is given, it is passed to Mail.defaults to alter settings
-  # of Mail
+  # of Mail.  Unless you set Mail.defaults elsewhere, you're going to want
+  # to do it here.
+  #
+  # === Example for IMAP and Gmail:
+  #
+  #   MailReader.new do
+  #     retriever_method :imap, { :address             => "imap.googlemail.com",
+  #                               :port                => 993,
+  #                               :user_name           => '<username>',
+  #                               :password            => '<password>',
+  #                               :enable_ssl          => true }
+  #   end
   def initialize(&settings)
     Mail.defaults(&settings) if block_given?
   end
