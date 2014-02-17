c888d78082627247e44e9dc82dfba65a9fb68570
Garry Dolley <gdolley@arpnetworks.com>
2014-02-16 19:27:22 -0800

    Beginning of Abuse Mail Monitor

---
+++ abuse_mail_monitor.rb
@@ -0,0 +1,17 @@
+# Abuse Mail Monitor
+#
+# Copyright ⓒ 2014 ARP Networks, Inc.
+#
+# :Author: Garry Dolley
+# :Created: 02-16-2014
+#
+# When we receive complaints of abuse (e.g. SPAM, DoS attack, etc...) and that
+# abuse can be identified as originating from a particular IP address, we
+# forward the complaint to a special email address monitored by this script.
+# This monitor can then lookup the customer / owner of the IP and forward the
+# complaint.
+
+require 'mail_monitor'
+require 'arpnet_mail_config' # Site specific
+
+monitor = MailMonitor.new(60, &ARPNET_MAIL_CONFIG)
