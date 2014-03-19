8a6a56180f03a1de594a404e31783b33779b0354
Garry Dolley <gdolley@arpnetworks.com>
2014-03-19 01:24:19 -0700

    Comment about options

--- mail_monitor.rb
+++ mail_monitor.rb
@@ -2,8 +2,8 @@ require 'mail_reader'
 
 # A basic class that retrieves all messages in a mailbox and yields each
 # message to a block; it then repeats this after @sleep_time, ad infinitum, or
-# a specified maximum number of iterations.  See MailReader for how to set up
-# mail retrieval settings.
+# a specified maximum number of iterations.  options is passed onto
+# MailReader#go.  See MailReader for how to set up mail retrieval settings.
 class MailMonitor < MailReader
   def initialize(sleep_time, options = {}, &settings)
     super(&settings)
