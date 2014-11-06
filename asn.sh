5d5e11fbc4df9fe6100f5142f80f40b31c2880ce
Garry Dolley <gdolley@arpnetworks.com>
2014-11-05 16:00:31 -0800

    Added asn.sh

---
+++ asn.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+#
+# Query CIDR REPORT for ASN info
+#
+# Created: 03-12-2009
+
+CACHE_DIR=~/.cache/ASNs
+
+usage() {
+  echo "$0 <ASN> [force-update]"
+  echo ""
+  echo "  <ASN>            ASN to query"
+  echo "  [force-update]   any non-empty string, forces cache refresh"
+}
+
+if [ -z "$1" ]; then
+  usage
+  exit 1
+fi
+
+ASN=$1
+
+if [ ! -d $CACHE_DIR ]; then
+  mkdir -p $CACHE_DIR
+fi
+
+AS_FILE="$CACHE_DIR/AS$ASN.txt"
+
+if [ -n "$2" ]; then
+  rm $AS_FILE
+fi
+
+if [ ! -f $AS_FILE ]; then
+  lynx -dump "http://www.cidr-report.org/cgi-bin/as-report?as=AS$ASN&v=4&view=2.0" > $AS_FILE
+fi
+
+less -F $AS_FILE
